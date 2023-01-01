10f8e979056c3fc5e667e660f53061d09e6f0cbc
s.zychkov <zychkov.serhii@gmail.com>
2023-01-01 21:18:57 +0200

    Fifth commit the end

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-window.addEventListener('scroll', e => {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
-} )
+window.addEventListener("scroll", (e) => {
+  document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+});
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+  wrapper: ".wrapper",
+  content: ".content",
+});
